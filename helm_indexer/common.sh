640c61d2346913a50feebc404b1c91639e34e9c3
Michael Timbrook <timbrook480@gmail.com>
2019-10-24 16:30:13 -0700

    Helm indexer resource base

---
+++ helm_indexer/common.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+exec 3>&1 # make stdout available as fd 3 for the result
+exec 1>&2 # redirect all output to stderr for logging
+
+loadPayload() {
+    payload=$(mktemp $TMPDIR/helm-resource-request.XXXXXX)
+    cat >$payload <&0
+}
+
+args() {
+    cat $payload
+}
+
+loadPayload
