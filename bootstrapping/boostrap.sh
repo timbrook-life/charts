8be8813db57cdc5cd0ff230dc2ae1096a8c9f837
Michael Timbrook <timbrook480@gmail.com>
2019-10-29 16:51:35 -0700

    Vault is packaged and bug fixes

--- bootstrapping/boostrap.sh
+++ bootstrapping/boostrap.sh
@@ -19,17 +19,23 @@ helm init --wait --upgrade --service-account tiller
 helm version
 
 kubectl create namespace core
+kubectl create namespace build
 
 # install Consul
 helm upgrade -i consul -f ./consul_values.yaml stable/consul --namespace=core
 
-read -p "Press enter when consul is ready..."
+# read -p "Press enter when consul is ready..."
 
 # install traefik as ingress controller
 helm upgrade -i traefik -f ./traefik_values.yaml stable/traefik --namespace=core
 
-read -p "Press enter when traefik is ready..."
+# read -p "Press enter when traefik is ready..."
 
 # install vault, custom container
 # TBD
-# helm upgrade -i vault -f ./vault_values.yaml personal/vault-1.0.0
+helm upgrade -i vault -f ./vault_values.yaml personal/vault
+
+# read -p "Press enter when vault is ready..."
+
+# Build system
+helm upgrade -i concourse -f ./concourse_values.yaml stable/concourse --namespace=build
--- kube_auth/common.sh
+++ kube_auth/common.sh
@@ -5,6 +5,7 @@ exec 1>&2 # redirect all output to stderr for logging
 loadPayload() {
     payload=$(mktemp $TMPDIR/helm-resource-request.XXXXXX)
     cat >$payload <&0
+    echo $payload
 }
 
 loadPayload
